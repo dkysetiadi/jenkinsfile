4b66c71adbc70035027e39b979306745ab8d5d2d
dkysetiadi <dkysetiadi@gmail.com>
2023-06-15 14:31:39 +0700

    add node

---
+++ mvnw2.cmd
@@ -0,0 +1,46 @@
+pipeline {
+    agent {
+        any {
+           label "lInux && java11"
+        }
+    }
+    stages {
+      stage("Build") {
+       steps {
+         echo ("Start Build")
+         sh("./mvnw clean compile test-compile")
+         echo ("Finish Build")
+           }
+        }
+        stage("Test") {
+       steps {
+         echo ("Start BUild"))
+         sh("./mvnw test")
+         echo ("FInish Build")
+           }
+        }
+        stage("Deploy") {
+       steps {
+         echo ("Hello Deploy1")
+         echo ("Hello Deploy2")
+         sleep(5)
+         echo ("Hello Deploy3")
+           }
+        }
+    }
+    post {
+  always {
+    echo "I will always say Hello again!"
+  }
+  success {
+    echo "Yay, success!"
+  }
+  failure {
+    echo "oh no, failure"
+  }
+  cleanup {
+    echo "Don't care success or error"
+  }
+}
+}
+
